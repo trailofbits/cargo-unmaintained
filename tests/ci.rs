7988a7d0355b9d05f5ef4a003f48bd6656cc75fb
Samuel Moelius <sam@moeli.us>
2023-10-17 21:00:54 -0400

    Add `ci` test

---
+++ tests/ci.rs
@@ -0,0 +1,94 @@
+use assert_cmd::Command;
+use regex::Regex;
+use std::env::remove_var;
+use tempfile::tempdir;
+
+#[ctor::ctor]
+fn initialize() {
+    remove_var("CARGO_TERM_COLOR");
+}
+
+#[test]
+fn clippy() {
+    Command::new("cargo")
+        .args([
+            "clippy",
+            "--all-features",
+            "--all-targets",
+            "--",
+            "--deny=warnings",
+            "--warn=clippy::pedantic",
+        ])
+        .assert()
+        .success();
+}
+
+#[test]
+fn dylint() {
+    Command::new("cargo")
+        .args(["dylint", "--all", "--", "--all-features", "--all-targets"])
+        .env("DYLINT_RUSTFLAGS", "--deny warnings")
+        .assert()
+        .success();
+}
+
+#[test]
+fn format() {
+    Command::new("cargo")
+        .args(["+nightly", "fmt", "--check"])
+        .assert()
+        .success();
+}
+
+#[test]
+fn license() {
+    let re = Regex::new(r"^[^:]*\b(Apache-2.0|BSD-3-Clause|ISC|MIT)\b").unwrap();
+
+    for line in std::str::from_utf8(
+        &Command::new("cargo")
+            .arg("license")
+            .assert()
+            .success()
+            .get_output()
+            .stdout,
+    )
+    .unwrap()
+    .lines()
+    {
+        if line == "Custom License File (1): ring" || line == "MPL-2.0 (1): uluru" {
+            continue;
+        }
+        assert!(re.is_match(line), "{line:?} does not match");
+    }
+}
+
+#[test]
+fn prettier() {
+    let tempdir = tempdir().unwrap();
+
+    Command::new("npm")
+        .args(["install", "prettier"])
+        .current_dir(&tempdir)
+        .assert()
+        .success();
+
+    Command::new("npx")
+        .args([
+            "prettier",
+            "--check",
+            &format!("{}/**/*.md", env!("CARGO_MANIFEST_DIR")),
+            &format!("{}/**/*.yml", env!("CARGO_MANIFEST_DIR")),
+            &format!("!{}/target/**", env!("CARGO_MANIFEST_DIR")),
+        ])
+        .current_dir(&tempdir)
+        .assert()
+        .success();
+}
+
+#[test]
+fn sort() {
+    Command::new("cargo")
+        .args(["sort", "--check"])
+        .assert()
+        .success();
+}
