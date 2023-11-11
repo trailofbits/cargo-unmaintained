39e2622114299204575b322f74bb3acdee993272
Samuel Moelius <sam@moeli.us>
2023-11-10 20:27:40 -0500

    Add `dogfood` test

---
+++ tests/dogfood.rs
@@ -0,0 +1,21 @@
+use snapbox::cmd::cargo_bin;
+use std::{env::remove_var, process::Command};
+
+mod util;
+use util::{tee, Tee};
+
+#[ctor::ctor]
+fn initialize() {
+    remove_var("CARGO_TERM_COLOR");
+}
+
+#[test]
+fn dogfood() {
+    let mut command = Command::new(cargo_bin("cargo-unmaintained"));
+    command.args(["unmaintained", "--color=never", "--verbose"]);
+
+    let output = tee(command, Tee::Stdout).unwrap();
+
+    assert!(output.status.success());
+    assert!(output.captured.is_empty());
+}
