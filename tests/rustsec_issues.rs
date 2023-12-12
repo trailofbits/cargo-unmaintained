c059b17ede29ead6a0f16d88476c99c402668a75
Samuel Moelius <35515885+smoelius@users.noreply.github.com>
2023-12-11 20:35:03 -0500

    Update rustsec_issues.rs

--- tests/rustsec_issues.rs
+++ tests/rustsec_issues.rs
@@ -1,5 +1,8 @@
 use snapbox::assert_matches_path;
-use std::{env::remove_var, process::Command};
+use std::{
+    env::{remove_var, var},
+    process::Command,
+};
 
 mod util;
 use util::{tee, token_modifier, Tee};
@@ -11,11 +14,16 @@ fn initialize() {
 
 #[test]
 fn rustsec_issues() {
+    // smoelius: If we are running on GitHub (i.e., `CI` is set), run only if `GITHUB_TOKEN_PATH` is
+    // also set. This is to avoid hitting API rate limits.
+    if var("CI").is_ok() && var("GITHUB_TOKEN_PATH").is_err() {
+        return;
+    }
+
     let mut command = Command::new("cargo");
     command
         .args(["run", "--bin=rustsec_issues"])
-        .current_dir("rustsec_util")
-        .env("RUST_BACKTRACE", "0");
+        .current_dir("rustsec_util");
 
     let output = tee(command, Tee::Stdout).unwrap();
 
