1388fd317292684e6fa5c3a5194f62acbc9aa7c2
Samuel Moelius <samuel.moelius@trailofbits.com>
2024-01-19 02:39:19 +0000

    Have `existence` return a `RepoStatus`

--- src/curl.rs
+++ src/curl.rs
@@ -1,14 +1,15 @@
-use anyhow::Result;
+use super::RepoStatus;
+use anyhow::{anyhow, Result};
 use curl::easy::Easy;
 
-pub(crate) fn existence(url: &str) -> Result<Option<bool>> {
+pub(crate) fn existence(url: &str) -> Result<RepoStatus<()>> {
     let mut handle = Easy::new();
     handle.url(url)?;
     handle.transfer().perform()?;
     let response_code = handle.response_code()?;
     match response_code {
-        200 => Ok(Some(true)),
-        404 => Ok(Some(false)),
-        _ => Ok(None),
+        200 => Ok(RepoStatus::Success(url, ())),
+        404 => Ok(RepoStatus::Nonexistent(url)),
+        _ => Err(anyhow!("unexpected response code: {response_code}")),
     }
 }
