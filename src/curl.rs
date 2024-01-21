f48814591ba0d3fccdf5ff823a9eee7fbe0a9d2f
Samuel Moelius <sam@moeli.us>
2024-01-20 20:50:22 -0500

    Treat timeouts as nonexistence (fix #86)

--- src/curl.rs
+++ src/curl.rs
@@ -1,16 +1,21 @@
 use super::RepoStatus;
 use anyhow::{anyhow, Result};
 use curl::easy::Easy;
+use std::time::Duration;
+
+const TIMEOUT: u64 = 10; // seconds
 
 pub(crate) fn existence(url: &str) -> Result<RepoStatus<()>> {
     let mut handle = Easy::new();
     handle.url(url)?;
     handle.follow_location(true)?;
-    handle.transfer().perform()?;
-    let response_code = handle.response_code()?;
-    match response_code {
-        200 => Ok(RepoStatus::Success(url, ())),
-        404 => Ok(RepoStatus::Nonexistent(url)),
-        _ => Err(anyhow!("unexpected response code: {response_code}")),
+    handle.timeout(Duration::from_secs(TIMEOUT))?;
+    let result = handle.transfer().perform();
+    match result.and_then(|()| handle.response_code()) {
+        Ok(200) => Ok(RepoStatus::Success(url, ())),
+        Ok(404) => Ok(RepoStatus::Nonexistent(url)),
+        Err(err) if err.is_operation_timedout() => Ok(RepoStatus::Nonexistent(url)),
+        Ok(response_code) => Err(anyhow!("unexpected response code: {response_code}")),
+        Err(err) => Err(err.into()),
     }
 }
