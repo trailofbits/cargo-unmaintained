73999ab275c13e20cc243a6a8b03c1af07c5fdc0
Samuel Moelius <samuel.moelius@trailofbits.com>
2026-01-13 18:56:11 +0000

    Update `rustsec_issues` test

--- examples/rustsec_issues/octocrab_util.rs
+++ examples/rustsec_issues/octocrab_util.rs
@@ -12,6 +12,10 @@ pub(super) static RT: LazyLock<runtime::Runtime> = LazyLock::new(|| {
 });
 
 pub(crate) fn load_token(token: &str) -> Result<()> {
+    rustls::crypto::aws_lc_rs::default_provider()
+        .install_default()
+        .unwrap_or_default();
+
     RT.block_on(async {
         let octocrab = octocrab::Octocrab::builder()
             .personal_token(token.trim_end())
