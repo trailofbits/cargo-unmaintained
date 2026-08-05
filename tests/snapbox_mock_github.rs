1dc66a19e8a90fe599283edbbcdb20d58a8d445c
Samuel Moelius <sam@moeli.us>
2026-08-05 04:55:13 -0400

    Mark `mock_github` `snapbox` tests as externally influenced

--- tests/snapbox_mock_github.rs
+++ tests/snapbox_mock_github.rs
@@ -1,10 +1,18 @@
+#![cfg(feature = "test-ei")]
+
 // This test has no need of the `__mock_github` feature. The test binary does not link this
 // package's library. Rather, it runs the `cargo-unmaintained-with-mock-github` binary as a
 // subprocess. That binary is built separately, in the `mock_github` nested workspace, which enables
 // `__mock_github` on its dependency on this package's library.
 
+// smoelius: Even though this test uses the `cargo-unmaintained-with-mock-github` binary, it is
+// still "externally influenced". For example, a tested package's dependency could go from
+// unmaintained to maintained because its repository was updated. Such a change necessitated the
+// following commit:
+// https://github.com/trailofbits/cargo-unmaintained/commit/ca3242dec8e6fd2e8e3e7cc6021c32c02f34ec59
+
 #[cfg_attr(target_os = "windows", ignore = "dependencies differ from Linux/macOS")]
 #[test]
-fn snapbox() {
+fn snapbox_mock_github() {
     testing::snapbox::snapbox(false, false).unwrap();
 }
