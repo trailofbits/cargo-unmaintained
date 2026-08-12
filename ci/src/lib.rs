1cf6d51401be2c97d798e1c35c624184d7810b6b
Samuel Moelius <samuel.moelius@trailofbits.com>
2026-08-12 05:07:30 -0400

    Use Trail of Bits `supply_chain` package

--- ci/src/lib.rs
+++ ci/src/lib.rs
@@ -1,20 +1,10 @@
 #![cfg(test)]
 
 use assert_cmd::assert::OutputAssertExt;
-use elaborate::std::{
-    env::{current_dir_wc, var_wc},
-    fs::{read_to_string_wc, write_wc},
-    process::CommandContext,
-};
+use elaborate::std::{env::current_dir_wc, fs::read_to_string_wc};
 use regex::Regex;
 use similar_asserts::SimpleDiff;
-use std::{
-    ffi::OsStr,
-    ops::Range,
-    path::Path,
-    process::{Command, ExitStatus},
-    str::FromStr,
-};
+use std::{ffi::OsStr, ops::Range, process::Command};
 use tempfile::tempdir;
 use testing::split_at_cut_lines;
 use walkdir::WalkDir;
@@ -369,57 +359,5 @@ fn group_starts<S: AsRef<str>>(
 #[cfg_attr(target_os = "windows", ignore = "xdg does not work on Windows")]
 #[test]
 fn supply_chain() {
-    let mut command = Command::new("cargo");
-    command.args(["supply-chain", "update", "--cache-max-age=0s"]);
-    let _: ExitStatus = command.status_wc().unwrap();
-
-    let mut command = Command::new("cargo");
-    command.args(["supply-chain", "json", "--no-dev"]);
-    let assert = command.assert().success();
-
-    let stdout_actual = std::str::from_utf8(&assert.get_output().stdout).unwrap();
-    let mut value = serde_json::Value::from_str(stdout_actual).unwrap();
-    remove_avatars(&mut value);
-    let stdout_normalized = serde_json::to_string_pretty(&value).unwrap();
-
-    let path_buf = Path::new(env!("CARGO_MANIFEST_DIR")).join("supply_chain.json");
-
-    if enabled("BLESS") {
-        write_wc(path_buf, stdout_normalized).unwrap();
-    } else {
-        let stdout_expected = read_to_string_wc(&path_buf).unwrap();
-
-        assert!(
-            stdout_expected == stdout_normalized,
-            "{}",
-            SimpleDiff::from_str(&stdout_expected, &stdout_normalized, "left", "right")
-        );
-    }
-}
-
-fn remove_avatars(value: &mut serde_json::Value) {
-    match value {
-        serde_json::Value::Null
-        | serde_json::Value::Bool(_)
-        | serde_json::Value::Number(_)
-        | serde_json::Value::String(_) => {}
-        serde_json::Value::Array(array) => {
-            for value in array {
-                remove_avatars(value);
-            }
-        }
-        serde_json::Value::Object(object) => {
-            object.retain(|key, value| {
-                if key == "avatar" {
-                    return false;
-                }
-                remove_avatars(value);
-                true
-            });
-        }
-    }
-}
-
-fn enabled(key: &str) -> bool {
-    var_wc(key).is_ok_and(|value| value != "0")
+    supply_chain::check("ci/supply_chain.json");
 }
